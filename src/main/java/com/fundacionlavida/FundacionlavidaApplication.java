c457c5f1b73ee4da3e5de766df538caff42bc88b
gianbarbaro <gianlbarbaro@gmail.com>
2023-03-31 21:19:55 -0300

    first commit

---
+++ src/main/java/com/fundacionlavida/FundacionlavidaApplication.java
@@ -0,0 +1,13 @@
+package com.fundacionlavida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundacionlavidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundacionlavidaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundacionlavida/FundacionlavidaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundacionlavida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundacionlavidaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
